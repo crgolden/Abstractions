689d6af4ca0a4c94b6cd781b3fb2767cbed2bf5f
Chris Golden <crgolden@msn.com>
2019-06-06 06:31:14 -0500

    Adding `Prfiles` package

---
+++ Profiles/EntityProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace crgolden.Abstractions
+{
+    using AutoMapper;
+
+    public class EntityProfile : Profile
+    {
+        public EntityProfile()
+        {
+            CreateMap<Model, Entity>(MemberList.Destination)
+                .IncludeAllDerived()
+                .ForMember(dest => dest.Created, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}
